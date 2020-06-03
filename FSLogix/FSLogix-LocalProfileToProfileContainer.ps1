c2b9dd31b632a0f20996eaba556aba76a5ae3894
Deyda <59874195+Deyda@users.noreply.github.com>
2020-06-03 15:57:50 +0200

    Update FSLogix-LocalProfileToProfileContainer.ps1

--- FSLogix/FSLogix-LocalProfileToProfileContainer.ps1
+++ FSLogix/FSLogix-LocalProfileToProfileContainer.ps1
@@ -11,6 +11,9 @@ Test before using!!
   Rewrite Author:   Manuel Winkel <www.deyda.net>
   Creation Date:    2020-03-04
   Purpose/Change:
+  NOTE: This only works between like profile versions. eg. You can’t migrate your 2008R2 profiles to Server 2016 and expect it to work.
+        This requires using frx.exe, which means that FSLogix needs to be installed on the server that contains the profiles. The script will create the folders in the USERNAME_SID format, and set all proper permissions.
+        Use this script. Edit it. Run it (as administrator) from the Citrix server. It will pop up this screen to select what profiles to migrate.
 #>
 
 #########################################################################################
