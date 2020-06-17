804a331d8124b5cc4148b1da786ca522fd61faf9
Deyda <59874195+Deyda@users.noreply.github.com>
2020-06-17 07:54:34 +0200

    Update 2-register.ps1

--- MSIX App Attach/2-register.ps1
+++ MSIX App Attach/2-register.ps1
@@ -1,4 +1,14 @@
-#MSIX app attach registration sample
+<#
+.SYNOPSIS
+The script register the MSIX package in user context
+.DESCRIPTION
+Use this script to register the MSIX App Attach Container in the user context
+.NOTES
+  Version:        1.0
+  Author:         Manuel Winkel <www.deyda.net>
+  Creation Date:  2020-06-04
+  Purpose/Change:
+#>
 #region variables 
 $packageName = "<package name>" 
 
