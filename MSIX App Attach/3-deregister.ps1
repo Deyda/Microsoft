673bc0e84be2a2ce9e358c0856906538dc62dddf
Deyda <59874195+Deyda@users.noreply.github.com>
2020-06-17 07:55:27 +0200

    Update 3-deregister.ps1

--- MSIX App Attach/3-deregister.ps1
+++ MSIX App Attach/3-deregister.ps1
@@ -1,4 +1,14 @@
-#MSIX app attach deregistration sample
+<#
+.SYNOPSIS
+The script deregister the MSIX Package
+.DESCRIPTION
+Use this script to deregister the MSIX Package
+.NOTES
+  Version:        1.0
+  Author:         Manuel Winkel <www.deyda.net>
+  Creation Date:  2020-06-04
+  Purpose/Change:
+#>
 #region variables 
 $packageName = "<package name>" 
 #endregion
