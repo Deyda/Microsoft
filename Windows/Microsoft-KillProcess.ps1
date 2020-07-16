39087e71c9932121819e808478334e76afc5ef0a
Deyda <59874195+Deyda@users.noreply.github.com>
2020-07-16 22:21:36 +0200

    Update Microsoft-KillProcess.ps1

--- Windows/Microsoft-KillProcess.ps1
+++ Windows/Microsoft-KillProcess.ps1
@@ -1,3 +1,14 @@
+<#
+.SYNOPSIS
+This script kills a process, if this reaches a specific limit and write a log file
+
+.NOTES
+  Version:        1.0
+  Author:         Manuel Winkel <www.deyda.net>
+  Creation Date:  2020-07-16
+  Purpose/Change: 
+#>
+
 cls
 $procname = '<Process NAme>'
 $cpu_usage=<CPU Limit>
