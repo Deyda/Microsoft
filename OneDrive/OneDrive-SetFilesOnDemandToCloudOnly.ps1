2b07dd49486d7dac41565987e0339e9cdae512d1
Deyda <59874195+Deyda@users.noreply.github.com>
2023-08-22 21:14:41 +0200

    Update OneDrive-SetFilesOnDemandToCloudOnly.ps1

--- OneDrive/OneDrive-SetFilesOnDemandToCloudOnly.ps1
+++ OneDrive/OneDrive-SetFilesOnDemandToCloudOnly.ps1
@@ -1,3 +1,18 @@
+<#
+.SYNOPSIS
+The script sets all files in OneDrive path to Cloud Only
+
+.DESCRIPTION
+Use this script to free space on your local system
+
+.NOTES
+  Version:        1.0
+  Author:         Manuel Winkel <www.deyda.net>
+  Creation Date:  2023-08-22
+  Purpose/Change:
+#>
+
+
 get-childitem $ENV:OneDriveCommercial -Force -File -Recurse -ErrorAction SilentlyContinue |
 Where-Object {$_.Attributes -match 'ReparsePoint' -or $_.Attributes -eq '525344' } |
 ForEach-Object {
