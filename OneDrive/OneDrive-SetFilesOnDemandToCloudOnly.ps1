91851c544c6cf3c0cd03096c253a57b60bdeafde
Deyda <59874195+Deyda@users.noreply.github.com>
2023-08-22 21:12:09 +0200

    Create OneDrive-SetFilesOnDemandToCloudOnly.ps1

---
+++ OneDrive/OneDrive-SetFilesOnDemandToCloudOnly.ps1
@@ -0,0 +1,5 @@
+get-childitem $ENV:OneDriveCommercial -Force -File -Recurse -ErrorAction SilentlyContinue |
+Where-Object {$_.Attributes -match 'ReparsePoint' -or $_.Attributes -eq '525344' } |
+ForEach-Object {
+    attrib.exe $_.fullname +U -P /s
+}
